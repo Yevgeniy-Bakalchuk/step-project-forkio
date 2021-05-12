6e906f3fd691ec57f81635c1950e11b5fc0321a8
MariiaSl <mvs030915@gmail.com>
2021-05-12 22:27:35 +0300

    button fixed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,3 +8,11 @@
 // icon.classList.toggle(".burger-menu-lines-active")
 // });
 
+const btnMenu = document.getElementById("btn-menu");
+const icon = document.querySelector(".burger-menu-lines");
+const menu = document.querySelector(".header_nav");
+
+btnMenu.addEventListener("click", () => {
+  icon.classList.toggle("burger-menu-lines-active");
+  menu.classList.toggle("header_nav-open");
+});
